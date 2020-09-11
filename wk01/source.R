501fa2b19f0d02b0f46efc191f94127c7722eb77
Christian <cmokoth@ncsu.edu>
2020-09-10 22:44:36 -0400

    read data in, establish source

--- wk01/source.R
+++ wk01/source.R
@@ -2,6 +2,6 @@
 library(tidyverse)
 library(readr)
 library(readxl)
-installed.packages()
+# installed.packages()
 # set directory
-setwd("C://Users//Public//Dropbox//My PC (LAPTOP-AUCK3O88)//Documents//RStudio-Mentor-Proj")
+# setwd("C://Users//Public//Dropbox//My PC (LAPTOP-AUCK3O88)//Documents//RStudio-Mentor-Proj")
