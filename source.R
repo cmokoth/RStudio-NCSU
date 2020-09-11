c9e5812fbfe4bc38737b6d56f681fe4b7ada3c28
Christian <cmokoth@ncsu.edu>
2020-09-10 22:11:17 -0400

    setting up data

---
+++ source.R
@@ -0,0 +1,7 @@
+# packages to load
+library(tidyverse)
+library(readr)
+library(readxl)
+installed.packages()
+# set directory
+setwd("C://Users//Public//Dropbox//My PC (LAPTOP-AUCK3O88)//Documents//RStudio-Mentor-Proj")
