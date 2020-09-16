47bb2872583232b36ff0079f53f2c2b095df169b
Christian <cmokoth@ncsu.edu>
2020-09-15 20:23:36 -0400

    ammend source

--- wk01/source.R
+++ wk01/source.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(readr)
 library(readxl)
 library(tidytext)
+library(ggplot2)
+library(broom)
 # installed.packages()
 # set directory
 # setwd("C:\Users\Public\User Documents\Documents\RStudio-NCSU")
