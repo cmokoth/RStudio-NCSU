37a078ab4b3f0794e2b8d4f7a7702d0beae2df26
Christian <cmokoth@ncsu.edu>
2020-09-25 09:35:46 -0400

    meeting

--- wk01/source.R
+++ wk01/source.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(readxl)
 library(tidytext)
 library(broom)
+library(stopwords)
 # installed.packages()
 # set directory
 # setwd("C:\Users\Public\User Documents\Documents\RStudio-NCSU")
